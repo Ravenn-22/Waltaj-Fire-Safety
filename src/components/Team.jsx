e67ea23753c84033e3d94c8eb4bb5574697b86dc
Shonaike Temitayo <temitayotijani02@gmail.com>
2026-09-04 16:19:21 -0700

    added niyi png

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -21,7 +21,7 @@ const engineers = [
     role: "Lead Fire Systems Engineer",
     badges: ["Addressable Systems", "Lagos State Licensed"],
     initials: "AA",
-    image: "",
+    image: "/images/niyi-waltaj.png",
   },
   {
     name: "Engr. Tijani Temitayo",
