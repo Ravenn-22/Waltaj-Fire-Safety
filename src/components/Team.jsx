c867c9041504fb9345c48154fb2b440ba5b61870
Shonaike Temitayo <temitayotijani02@gmail.com>
2026-09-04 16:29:39 -0700

    refixing

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -21,7 +21,7 @@ const engineers = [
     role: "Lead Fire Systems Engineer",
     badges: ["Addressable Systems", "Lagos State Licensed"],
     initials: "AA",
-    image: "/images/IMG_8417.png",
+    image: "/images/niyi.png",
   },
   {
     name: "Engr. Tijani Temitayo",
